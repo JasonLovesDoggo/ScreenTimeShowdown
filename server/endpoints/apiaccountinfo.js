12b967ba7cc1db8239de69e20b55d4160ae67b20
Patrick Lin <apocalypsecalculator@gmail.com>
2023-10-14 15:40:27 -0400

    add account info endpoint

---
+++ server/endpoints/apiaccountinfo.js
@@ -0,0 +1,26 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const axios = require('axios').default;
+const config = require('../config');
+const { nanoid } = require('nanoid');
+
+module.exports.name = "/api/account/info";
+module.exports.method = "GET";
+module.exports.verify = function (req, res) {
+    return !!req.user;
+}
+
+module.exports.execute = function (req, res) {
+    prisma.user.findUnique({
+        where: {
+            id: req.user.id
+        },
+        include: {
+            transactions: true,
+            groups: true
+        }
+    }).then(user => {
+        delete user.password;
+        res.json(user);
+    });
+}
