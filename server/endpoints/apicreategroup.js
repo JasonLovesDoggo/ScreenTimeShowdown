01f7c60c6ea8f149d57d808d53de3defe39f3783
Cqctxs <seanzhao2007@gmail.com>
2023-10-14 02:18:57 -0400

    Create Group endpoint

---
+++ server/endpoints/apicreategroup.js
@@ -0,0 +1,41 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const nanoid = require('nanoid');
+
+module.exports.name = "/api/creategroup";
+module.exports.method = "PUT";
+module.exports.verify = function (req, res) {
+    return req.user;
+}
+
+module.exports.execute = function (req, res) {
+    if (req.body.name && req.body.bet && req.body.interval) {
+        console.log("valid request");
+        let groupid = nanoid.nanoid(16);
+        prisma.group.create({
+            data: {
+                id: groupid,
+                name: req.body.name,
+                users: {
+                    connect: [{
+                        id: req.user.id
+                    }]
+                },
+                startdate: '0',
+                enddate: '0',
+                interval: req.body.interval,
+                bet: req.body.bet,
+                pot: 0,
+                logs: {},
+                invites: {}
+            }
+        }).then((group) => {
+            res.json({ message: 'success!', group: group });
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json({ error: "Internal server error" })
+        });
+    } else {
+        res.status(400).json({ error: `Invalid form` });
+    }
+}
