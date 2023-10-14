6f6a51b7d49f93cb9b01c34cb4c2928acf14fd67
Patrick Lin <apocalypsecalculator@gmail.com>
2023-10-14 00:38:49 -0400

    change to sync

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -2,21 +2,19 @@ const jwt = require('jsonwebtoken');
 const secret = require('../secret');
 
 module.exports.authorization = (req, res, next) => {
-    return new Promise((resolve, reject) => {
-        if (req.headers.authorization && req.headers.authorization.split(' ').length == 2) {
-            let token = req.headers.authorization.split(' ')[1];
-            jwt.verify(token, secret.jwt, (err, user) => {
-                if (err) {
-                    next();
-                }
-                else {
-                    req.user = user;
-                    next();
-                }
-            })
-        }
-        else {
-            next();
-        }
-    })
+    if (req.headers.authorization && req.headers.authorization.split(' ').length == 2) {
+        let token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, secret.jwt, (err, user) => {
+            if (err) {
+                next();
+            }
+            else {
+                req.user = user;
+                next();
+            }
+        })
+    }
+    else {
+        next();
+    }
 }
