9bb7a7d12b594ae4366b5d0516c16479b89467b6
Jason Cameron <jasoncameron.all@gmail.com>
2023-10-14 20:42:07 -0400

    fix: me being stupid

--- server/endpoints/oauthauthorize.js
+++ server/endpoints/oauthauthorize.js
@@ -4,7 +4,7 @@ module.exports.verify = function (req, res) {
     return true;
 }
 module.exports.execute = async function (req, res) {
-    req.redirect('https://api.instagram.com/oauth/authorize' +
+    res.redirect('https://api.instagram.com/oauth/authorize' +
         `?client_id=${process.env.INSTACLIENTID}` +
         `&redirect_uri=${process.env.INSTAREDIRECT}` +
         `&response_type=code` +
