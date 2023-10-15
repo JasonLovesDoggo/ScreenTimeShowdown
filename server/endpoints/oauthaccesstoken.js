c1789e241a29295b8af946ab570ecd0188646e25
Jason Cameron <jasoncameron.all@gmail.com>
2023-10-14 22:21:05 -0400

    updated url

--- server/endpoints/oauthaccesstoken.js
+++ server/endpoints/oauthaccesstoken.js
@@ -6,15 +6,20 @@ module.exports.verify = function (req, res) {
     return true;
 }
 module.exports.execute = async function (req, res) {
+    let code = req.query.code;
+    console.log(code);
+    code = code.substring(0, code.length - 2);
+    console.log(code);
+
     let data = {'client_id' : instaclientid,
         'client_secret' : instaclientsecret,
         'grant_type' : 'authorization_code',
         'redirect_uri' : instaredirect,
-        'code' : req.query.code
+        'code' : code,
     };
 
 // Configure the request
-    post('https://api.instagram.com/oauth/access_token', data)
+    post('https://api.instagram.com/oauth/access_token', )//todo this micleaght be the issue\???
         .then(function (response) { // handle success
             console.log(JSON.parse(response.data));
         })
