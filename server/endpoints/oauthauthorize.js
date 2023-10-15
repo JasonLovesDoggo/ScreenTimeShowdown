681ba94ed980708b7438eaef5568ff483d4f2f6f
Jason Cameron <jasoncameron.all@gmail.com>
2023-10-15 01:52:54 -0400

    updated redirect link

--- server/endpoints/oauthauthorize.js
+++ server/endpoints/oauthauthorize.js
@@ -6,7 +6,7 @@ module.exports.verify = function (req, res) {
 }
 module.exports.execute = async function (req, res) {
     if (!req.userid) {
-        return res.redirect('https://screentimeshowdown.tech/login?redirect=/oauth/authorize')
+        return res.redirect('https://screentimeshowdown.tech/login?next=/oauth/authorize')
     }
     res.redirect('https://api.instagram.com/oauth/authorize' +
         `?client_id=${config.instaclientid}` +
