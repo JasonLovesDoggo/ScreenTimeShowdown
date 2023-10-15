892ac72532232ec9fd7b2793e1a8971b17de2e43
Patrick Lin <apocalypsecalculator@gmail.com>
2023-10-14 21:37:26 -0400

    tweak prompts

--- server/lib/generate.js
+++ server/lib/generate.js
@@ -5,11 +5,11 @@ cohere.init(config.cohere.apikey);
 
 module.exports.generate = async (name, user, day, n, maxtokens) => {
     let prompts = [
-      //  `Use coarse language to bully ${name} for failing to break their addiction of scrolling Instagram Reels (in 3 sentences)`,
-        `In a game to see who can last longest without social media, ${name} has started scrolling Instagram Reels on day ${day}. Write a short blurb to shame them using coarse language.`,
-        `In a game to see who can last longest without social media, ${name} has started scrolling Instagram Reels on day ${day}. Write a short blurb to reprimand them using compassionate language.`,
+        `Use coarse language to bully ${name} for failing to break their addiction of scrolling Instagram Reels (in 3 sentences)`,
+        `In a game to see who can last longest without social media, ${name} has started scrolling Instagram Reels on day ${day}. Write a short blurb to shame them using coarse language in 3 sentences.`,
+        `In a game to see who can last longest without social media, ${name} has started scrolling Instagram Reels on day ${day}. Write a short blurb to reprimand them using compassionate language in 3 sentences.`,
         `Write a short blurb to congratulate ${name} from breaking their addiction to scrolling Instagram Reels`,
-        `Congratulate ${name} for winning the pot   in a bet to break social media addiction`
+        `Congratulate ${name} for winning the pot in a bet to break social media addiction in under 3 sentences`
     ];
 
     const response = await cohere.generate({
