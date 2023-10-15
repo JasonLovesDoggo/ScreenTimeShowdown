89fcd409bbf2d088b50da1ae7f6833577249f7a6
Jason Cameron <jasoncameron.all@gmail.com>
2023-10-14 23:14:08 -0400

    testing a form impl.

--- server/endpoints/oauthaccesstoken.js
+++ server/endpoints/oauthaccesstoken.js
@@ -9,15 +9,15 @@ module.exports.execute = async function (req, res) {
     let code = req.query.code;
     console.log(code);
 
-    let data = {client_id : instaclientid,
-        client_secret : instaclientsecret,
-        grant_type : 'authorization_code',
-        redirect_uri : instaredirect,
-        code : code,
-    };
+    let dataForm = new FormData();
+    dataForm.append('client_id', instaclientid);
+    dataForm.append('client_secret', instaclientsecret);
+    dataForm.append('grant_type', 'authorization_code');
+    dataForm.append('redirect_uri', instaredirect);
+    dataForm.append('code', code);
 
 // Configure the request
-        post('https://api.instagram.com/oauth/access_token', data)//todo the data param might be the issue\???
+    post('https://api.instagram.com/oauth/access_token', dataForm)//todo the data param might be the issue\???
         .then(function (response) { // handle success
             console.log(JSON.parse(response.data));
         })
@@ -25,19 +25,19 @@ module.exports.execute = async function (req, res) {
             console.log(response);
         });
 
-/*
+    /*
 
 
-        // pbody should look like this:
-        // {"access_token":"8943851.83434d.697342341324jkfdjsf41afd784932a2e8",
-        //   "user":
-        //     {"username":"my_user_name",
-        //     "bio":"blah blah...",
-        //     "website":"http:\/\/www.something.com",
-        //   "profile_picture":"http:\/\/images.ak.instagram.com\/profiles\/profile_851_73sq_115.jpg",
-        //     "full_name":"Full Name",
-        //     "id":"8943851"}
-        //   }
+            // pbody should look like this:
+            // {"access_token":"8943851.83434d.697342341324jkfdjsf41afd784932a2e8",
+            //   "user":
+            //     {"username":"my_user_name",
+            //     "bio":"blah blah...",
+            //     "website":"http:\/\/www.something.com",
+            //   "profile_picture":"http:\/\/images.ak.instagram.com\/profiles\/profile_851_73sq_115.jpg",
+            //     "full_name":"Full Name",
+            //     "id":"8943851"}
+            //   }
 
-    }); */
+        }); */
 }
