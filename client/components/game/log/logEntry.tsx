23875036bc64a344a138eac1d31d25da3db27ad1
ji-mmyliu <jimmyjcl753@gmail.com>
2023-10-15 09:46:55 -0400

    Add title to game logs

--- client/components/game/log/logEntry.tsx
+++ client/components/game/log/logEntry.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { GroupLog } from "../../../util/models";
 
 export const GameLogEntry = ({ logEntry }: { logEntry: GroupLog }): JSX.Element => {
     return (
         <Box sx={{ borderRadius: "10px", backgroundColor: "white", padding: "5px" }}>
-            {logEntry.content}
+            <Typography variant="body1" fontWeight='400'>{logEntry.title}:</Typography>
+            <Typography variant="body1">{logEntry.content}</Typography>
         </Box>
     );
 }
