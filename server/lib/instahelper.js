fbe4b2bb0a68f75ef0c1bf952be09dfa2cfacc3b
Cqctxs <seanzhao2007@gmail.com>
2023-10-15 01:00:49 -0400

    Created instagram methods

---
+++ server/lib/instahelper.js
@@ -0,0 +1,33 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+module.exports.instahelper = async (uid, instaid, lastonline) => {
+    try {
+        await prisma.insta.create({
+            data: {
+                id: instaid,
+                lastonline: lastonline,
+                userid: uid 
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports.getuser = async (instaid) => {
+    try {
+        const instauser = await prisma.insta.findUnique({
+            where: {
+                id: instaid
+            },
+            include: {
+                userid: true
+            }
+        })
+        return instauser.id;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
